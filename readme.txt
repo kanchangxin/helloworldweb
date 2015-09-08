a837299e5afe466e4e0c7e1943b2a987767c6493
kanchangxin <kanchangxin@139.com>
2015-09-08 17:59:53 +0800

    新建

---
+++ readme.txt
@@ -0,0 +1,2 @@
+用于测试系统环境是否可用。
+存粹使用java web技术，打印hello world
---
+++ src/helloworldweb/PrintHello.java
@@ -0,0 +1,40 @@
+package helloworldweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PrintHello extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+
+	/**
+	 * @param req
+	 * @param resp
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<HTML><HEAD><TITLE>My First Servlet</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("<H1>Hello </H1>");
+		out.println("</BODY></HTML>");
+		out.close();
+		System.out.println(" ================ say hello is done ================");
+	}
+
+}
